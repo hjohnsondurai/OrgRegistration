eb9f87c9bb934c2e2c31295dfb4f1e59dbac2bbf
hjohnsondurai@gmail.com <hjohnsondurai@gmail.com>
2020-10-24 14:04:43 +0530

    Client: Adding Registration component and updated its routing path

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './components/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: "register", component: RegistrationComponent
+  },
+  {
+    path: '', redirectTo: 'register', pathMatch: 'full'
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationComponent } from './components/registration/registration.component';
 import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
