3797bfbfe7944c5786a77c42f106c7edc939c446
hjohnsondurai@gmail.com <hjohnsondurai@gmail.com>
2020-10-24 13:35:18 +0530

    Server: Added rest api to send OTP from server

--- server/app.js
+++ server/app.js
@@ -30,6 +30,36 @@ let path_public = path.join(path_app, 'public');
 
 app.use(express.static(path_public));
 
+// If we wish to serve the client application's file from the serve
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(path_public, 'index.html'));
+// });
+
+let mailConfig = require('./config.json');
+
+mailer.extend(app, mailConfig);
+
+app.post("/sendOTP", function (req, res, next) {
+    try{
+        if (req.body['to'] == undefined || req.body['subject'] == undefined || req.body['username'] == undefined || req.body['otp'] == undefined) {
+            res.send(400, { SERVER_RESPONSE: 0, SERVER_MESSAGE: "Invalid data!" });
+            return;
+        }
+
+        app.mailer.send('otp', req.body, function (err, message) {
+            if (err) {
+                console.log(err);
+                res.json({ SERVER_RESPONSE: 0, SERVER_MESSAGE: 'There was an error sending the email' });
+                return;
+            }
+            return res.json({ SERVER_RESPONSE: 1, SERVER_MESSAGE: 'Email has been sent!' });
+        });
+    }
+    catch(err){
+        res.send(400, { SERVER_RESPONSE: 0, SERVER_MESSAGE: "Server process failed!" });
+    }
+});
+
 server.listen(process.env.PORT || 1337, function (err) {
     if (!err)
         console.log('Successfully started server at ' + (process.env.PORT || 1337) + ' in ' + process.env.NODE_ENV + ' environment.');
