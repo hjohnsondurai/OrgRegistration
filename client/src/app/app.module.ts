eff7a10096a87fc88069074c25332f5ee536c8e3
hjohnsondurai@gmail.com <hjohnsondurai@gmail.com>
2020-10-24 14:21:10 +0530

    Client: Designed UI for Organisation registration form with stepper and form controls

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { DataService } from './services/data.service';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { NgOtpInputModule } from 'ng-otp-input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -15,6 +28,19 @@ import { DataService } from './services/data.service';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatStepperModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatRadioModule,
+    HttpClientModule,
+    MatSelectModule,
+    FormsModule,
+    MatCheckboxModule,
+    NgOtpInputModule,
+    MatSnackBarModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
--- client/src/app/components/registration/registration.component.ts
+++ client/src/app/components/registration/registration.component.ts
@@ -1,15 +1,170 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import * as $ from 'jquery';
+
+let self;
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-
-  constructor() {
+  countriesList = [];
+  selCountry = [];
+  // isOptional = false;
+  stepSelIndex = 0;
+  enteredOTP = "";
+  genOTP = {
+    code: "",
+    time: null,
+    timerCount: 30,
+    isTimerExpired: true,
+    timerStart: function () {
+      var _that = this;
+      self.genOTP.isTimerExpired = false;
+      var refreshIntervalId = setInterval(function () {
+        self.genOTP.timerCount -= 1;
+        if (_that.timerCount <= 0) {
+          clearInterval(refreshIntervalId);
+          self.genOTP.isTimerExpired = true;
+        }
+        console.log(self.genOTP.timerCount)
+      }, 1000);
+    }
+  }
+  data = {
+    personal: {
+      name: "",
+      gender: '',
+      country: '',
+      state: '',
+      phone: ''
+    },
+    company: {
+      orgLogo: '',
+      name: '',
+      email: '',
+      jobTitle: '',
+      category: '',
+      isAcceptedTerms: false
+    }
+  }
+  stepper = {
+    step1Success: false,
+    step2Success: false,
+    step3Success: false
+  };
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {
+    this.getStatesByCountries();
+    self = this;
   }
 
   ngOnInit(): void {
   }
+
+  getStatesByCountries() {
+    this.http.get("assets/data/statesByCountries.json").subscribe((data) => {
+      this.countriesList = data["countries"];
+    }, (err) => {
+      console.log(err)
+    });
+  }
+  generateOTP() {
+    var digits = '0123456789';
+    var otpLength = 4;
+    var otp = '';
+    for (let i = 1; i <= otpLength; i++) {
+      var index = Math.floor(Math.random() * (digits.length));
+      otp = otp + digits[index];
+    }
+    return otp;
+  }
+
+  sendMail() {
+    this.genOTP.code = this.generateOTP();
+    this.genOTP.time = new Date();
+    this.genOTP.timerStart();
+    const params = {
+      to: this.data.company.email,
+      subject: "Registration Email Verfication OTP",
+      otp: this.genOTP.code,
+      username: this.data.personal.name
+    }
+
+    $.ajax({
+      url: "http://localhost:1337/sendOTP",
+      type: "POST",
+      crossDomain: true,
+      data: params,
+      success: function (data) {
+        console.log(data);
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
+  }
+
+  showMessage(message) {
+    this.snackBar.open(message, "Hide", {
+      duration: 2000,
+    });
+  }
+
+  // Binded functions
+  updateCountry(country) {
+    this.selCountry = country;
+  }
+
+  uploadOrgLogo(event) {
+    var file = event.target.files[0],
+      imageType = /image.*/;
+
+    if (!file.type.match(imageType))
+      alert("Selected file is not a image type. Please select a image file");
+
+    var reader = new FileReader();
+    reader.onload = fileOnload;
+    reader.readAsDataURL(file);
+
+    // Event handler
+    function fileOnload(e) {
+      let dataURL = e.target.result;
+      self.data.company.orgLogo = dataURL;
+    }
+  }
+
+  submitPersonalDetails() {
+    console.log(this.data);
+    var personal = this.data.personal;
+    if (personal.name == "" || personal.gender == "" || personal.country == "" || personal.state == "" || personal.phone == "") {
+      this.showMessage("Please fill the required fields");
+      this.stepSelIndex = 0;
+    }
+    else {
+      this.stepSelIndex = 1;
+    }
+
+  }
+
+  submitOriganisationDetails() {
+    var company = this.data.company;
+    if (company.name == "" || company.email == "" || company.category == "" || company.jobTitle == "" || company.orgLogo == "" || !company.isAcceptedTerms) {
+      this.showMessage("Please fill the required fields");
+      this.stepSelIndex = 1;
+    }
+    else {
+      this.sendMail();
+      this.stepSelIndex = 2;
+    }
+    console.log(this.data);
+  }
+
+  onOtpChange(event) {
+    if (event.length == 4)
+      this.enteredOTP = event.length
+  }
+
 }
